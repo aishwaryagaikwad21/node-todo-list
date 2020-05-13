bd7c5209f83091bd24c376f1c5ecffb3e817378f
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-05-13 17:30:55 +0530

    todocontroller

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var todoController = require('./controllers/todocontroller')
 var app = express();
 
 app.listen(3000);
@@ -9,6 +9,8 @@ app.set('view engine','ejs');
 
 //static file
 app.use(express.static('./public'));
+//app.use(express.static('./public'));
+todoController(app);
 
 console.log('listening to port 3000');
 
