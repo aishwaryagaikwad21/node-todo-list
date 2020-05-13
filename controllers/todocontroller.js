85dbea2b8d693e5e93222e1b41ae858165a70eb2
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-05-13 18:30:10 +0530

    1.2

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,11 +1,16 @@
+var data = [{item:'do exercise'},{item:'complete assignment'},{item:'watch friends'}];
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
 module.exports = function(app){
 
     app.get('/todo',function(req,res){
-        res.render('todo');
+        res.render('todo',{todos:data});
     });
 
-    app.post('/todo',function(req,res){
-        
+    app.post('/todo', urlencodedParser, function(req,res){
+        data.push(req.body);
+        res.json(data);
+        console.log(req.body);
     });
 
     app.delete('/todo',function(req,res){
