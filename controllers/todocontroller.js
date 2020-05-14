dc34949d2e1026e9b18586fe041e249819c04a7f
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-05-14 12:49:43 +0530

    complete

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -17,27 +17,34 @@ var todoSchema = new mongoose.Schema({
     item:String
 });
 var Todo = mongoose.model('todo',todoSchema);
-var itemOne = Todo({item:'sleep'}).save(function(err){
-    if(err) throw err;
-    console.log('item saved');
-})
+
 
 module.exports = function(app){
 
     app.get('/todo',function(req,res){
-        res.render('todo',{todos:data});
+        //get data from mongodb and pass it to view
+        Todo.find({},function(err,data){
+            if (err) throw err;
+            res.render('todo',{todos:data});
+        });
+        
     });
 
     app.post('/todo', urlencodedParser, function(req,res){
-        data.push(req.body);
-        res.json(data);
+        //get data from view and add to database
+        var newTodo = Todo(req.body).save(function(err,data){
+            if(err) throw err;
+            res.json(data);
+        });
+        
         //console.log(req.body);
     });
 
     app.delete('/todo/:item', function(req,res){
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        //delete requested item from mongodb
+        Todo.find({item:req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+            if (err) throw err;
+            res.json(data);
         });
-        res.json(data);
     });
 }
