3975d7e10e09284f534e02ec9149bbc971ad593b
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2020-05-14 12:26:55 +0530

    storeindb

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,6 +1,27 @@
 var data = [];
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
+var mongoose = require("mongoose"); //connecting node js environment with mongobd
+mongoose.connect("mongodb://localhost:27017/todo",{ useUnifiedTopology: true },(error)=>{
+    if(!error)
+    {
+        console.log("Successfully connected to database");
+    }
+    else
+    {
+        console.log("Error connecting to database.")
+    }
+});
+
+var todoSchema = new mongoose.Schema({
+    item:String
+});
+var Todo = mongoose.model('todo',todoSchema);
+var itemOne = Todo({item:'sleep'}).save(function(err){
+    if(err) throw err;
+    console.log('item saved');
+})
+
 module.exports = function(app){
 
     app.get('/todo',function(req,res){
